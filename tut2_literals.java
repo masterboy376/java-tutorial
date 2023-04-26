2a7d65ce86694fe3b60f413f495227fc1d141e8b
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-26 16:09:20 +0530

    Create tut2_literals.java

---
+++ tut2_literals.java
@@ -0,0 +1,10 @@
+  char section = 'a';
+
+        char age2 = 'A';
+        float age = 5.893f;
+        double point = 3.2384922987d;
+        long extensive = 53093478983675848L;
+        String str = "sam";
+        System.out.println(str);
+        System.out.println(age+point-extensive);
+;
