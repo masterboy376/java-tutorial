04afc248e630447db28cc60fd1f214b518f4377a
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-26 16:13:31 +0530

    Update tut1_datatypes_and_variables.java

--- tut1_datatypes_and_variables.java
+++ tut1_datatypes_and_variables.java
@@ -1,76 +1,84 @@
- byte x = 5;
+package com.company;
 
-int y = 6;
-short z = 7;
-float a = 6.7f + x;//result ---> float or double
-//System.out.println(a);
-char c = 'A';
-int b = c+x;
-System.out.println(b);
+import java.util.Scanner;
 
-// Increment and decrement Operators // ++: increment ; --:decrement
-int i = 50;
-//int b = i++; // first b is assigned i then i is incremented.
-int j = 40;
-//int c = ++j; // first j is incremented then c is assigned j.
-System.out.println(i++);
-System.out.println(i);
-System.out.println(++i);
-System.out.println(i);
+public class Exercise1 {
+    public static void main(String[] args) {
+        byte x = 5;
 
-// note
-char a = 'A';
-System.out.println(a++);
-System.out.println(a);
-System.out.println(++a);
-System.out.println(a);
-System.out.println(a++);
-System.out.println(a);
-System.out.println(++a);
-System.out.println(a);
-System.out.println(a++);
-System.out.println(a);
-System.out.println(++a);
-System.out.println(a++);
-System.out.println(a);
-System.out.println(++a);
-System.out.println(a);
-System.out.println(a++);
-System.out.println(a);
-System.out.println(++a);
-System.out.println(a);
-System.out.println(a++);
-System.out.println(a);
-System.out.println(++a);
-System.out.println(a);
-System.out.println(a++);
-System.out.println(a);
-System.out.println(++a);
-System.out.println(a);
-System.out.println(a++);
-System.out.println(a);
-System.out.println(++a);
-System.out.println(a);
-System.out.println(a++);
-System.out.println(a);
-System.out.println(++a);
-System.out.println(a);
-System.out.println(a++);
-System.out.println(a);
-System.out.println(++a);
-System.out.println(a);
-System.out.println(a++);
-System.out.println(a);
-System.out.println(++a);
-System.out.println(a);
-System.out.println(a++);
-System.out.println(a);
-System.out.println(++a);
-System.out.println(a);
-System.out.println(a++);
-System.out.println(a);
+        int y = 6;
+        short z = 7;
+        float a = 6.7f + x;//result ---> float or double
+        //System.out.println(a);
+        char c = 'A';
+        int b = c+x;
+        System.out.println(b);
 
-// Quick quiz
-int y = 7;
-int x = ++y * 8;
-System.out.println(x);
+        // Increment and decrement Operators // ++: increment ; --:decrement
+        int i = 50;
+        //int b = i++; // first b is assigned i then i is incremented.
+        int j = 40;
+        //int c = ++j; // first j is incremented then c is assigned j.
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+
+        // note
+        char a = 'A';
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+
+        // Quick quiz
+        int y = 7;
+        int x = ++y * 8;
+        System.out.println(x);
+    }
+}
