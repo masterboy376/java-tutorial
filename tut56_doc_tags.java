bfa6fd86f235d3a7c2f8b0966d5b91475071aba2
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-26 20:51:15 +0530

    Create tut56_doc_tags.java

---
+++ tut56_doc_tags.java
@@ -0,0 +1,22 @@
+ package com.company;
+
+/**
+ * This class is to demonstrate what javadoc is and how it is used in the java industry.
+ * This is <i>itallic</i> word<p>This is new paragraph.</p>
+* @author Sambhav (Sam)
+* @version 0.1
+* @since 2004
+* @see <a href="https://docs.oracle.com/en/java/javase/16/docs/api/index.html" target ="_blank"></a>Java Docs
+ */
+
+public class javadoc_106 {
+
+    public void add(int a, int b){
+        System.out.println("This sum is: "+ a+b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is my main method.");
+
+    }
+}
