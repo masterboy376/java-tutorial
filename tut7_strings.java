631c19d619388e4c5db969bec6dd6660db8d0871
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-26 17:58:25 +0530

    Create tut7_strings.java

---
+++ tut7_strings.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+public class Strings {
+    public static void main(String[] args) {
+        /*String s = new String("Sambhav");
+        //String s = "Sambhav";
+        System.out.print("The name is: ");
+        System.out.println(s);*/
+
+        int a = 6;
+        float b = 5.657f;
+        System.out.printf("The value of a is %d and value of b is %.2f", a, b);
+        System.out.format("The value of a is %8d and value of b is %8.9f", a, b);
+
+        /*Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        //String st1 = sc.nextLine();
+        System.out.println(st);
+        //System.out.println(st1);*/
+    }
+}
