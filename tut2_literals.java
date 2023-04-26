58511467883e131411354073c0ca2af0bd3209f8
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-26 17:45:45 +0530

    Update tut2_literals.java

--- tut2_literals.java
+++ tut2_literals.java
@@ -1,5 +1,10 @@
-  char section = 'a';
+package com.company;
 
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        char section = 'a';
         char age2 = 'A';
         float age = 5.893f;
         double point = 3.2384922987d;
@@ -7,4 +12,5 @@
         String str = "sam";
         System.out.println(str);
         System.out.println(age+point-extensive);
-;
+    }
+}
