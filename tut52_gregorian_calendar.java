404e449c75d69bb8bc0b63505dcc5f28cef9d172
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-26 20:45:45 +0530

    Create tut52_gregorian_calendar.java

---
+++ tut52_gregorian_calendar.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+import java.util.*;
+
+public class GregorianCalendarClass_99 {
+    public static void main(String[] args) {
+        GregorianCalendar gc = new GregorianCalendar();
+        System.out.println(gc.isLeapYear(1900));
+
+
+    }
+}
