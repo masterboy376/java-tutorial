a35aea5ebc94037493ac948f921b40bbdd0051fe
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-26 20:35:16 +0530

    Update tut45_finally_block.java

--- tut45_finally_block.java
+++ tut45_finally_block.java
@@ -1,49 +1,66 @@
 package com.company;
 
 
-class NegativeRadiusException extends Exception{
-    @Override
-    public String toString(){
-        return  "Radius cannot be negative.";
-    }
-}
-
-public class ThrowAndThrows_84 {
-    // 1
-    public static  double  Area (int r ) throws NegativeRadiusException {// throws -> give indication of Exception
-        if(r<0){
-            throw new NegativeRadiusException();// throw -> throw actual Exception
-        }
-        double result = Math.PI*r*r;
-        return result;
-    }
-    //2
-    public static int divide(int a, int b) throws ArithmeticException{
-        // made by sam
-        /*if(b==0){
-            throw new ArithmeticException();
-        }*/ // it do not matter here because ArithmeticException is a predefined Exception, and only executes when b==0.
-        int result = a/b;
-        return result;
-    }
-
-    public static void main(String[] args) {
-        // 1
+public class FinallyBlock_85 {
+    public static int greet(){
         try {
-            double area = Area(-8);
-            System.out.println(area);
-        }
-        catch(Exception e){
-            System.out.println("Exception : "+e);
-        }
-        // 2
-        // tom  -  uses divide function created by sam.
-        try {
-            int c = divide(6, 0);
-            System.out.println(c);
+            int a = 50;
+            int b = 10;
+            int c = a/b;
+            return c;
         }
         catch(Exception e ){
-            System.out.println("Exception : "+e);
+            System.out.println(e);
         }
+        finally{
+            System.out.println("Clean up... Program ended");
+        }
+        System.out.println("Clean up... Program ended");// if try executes then this will not run bcoz it is not in finally block.
+        return -1;
+    }
+    public static void main(String[] args) {
+        /*try {
+            int a = 5;
+            int b = 0;
+            int c = a/b;
+        }
+        catch(Exception e ){
+            System.out.println(e);
+        }
+        finally{// here finally is useless
+            System.out.println("Program ended");
+        }*/
+        System.out.println(greet());
+        //--------------------------------------------------------------------------------------------------------------
+        // quiz
+        int a = 7;
+        int b = 9;
+        while(true){
+            try{
+                System.out.println(a/b);
+            }
+            catch(Exception e ){
+                System.out.println(e);
+                break;
+            }
+            finally{
+                System.out.println("I am finally, value of b = "+b);
+            }
+            b--;
+        }
+       //---------------------------------------------------------------------------------------------------------------
+        try{
+            System.out.println(58/7);
+        }
+        finally{
+            System.out.println("done 1");
+        }// -----------------------------> valid
+
+        try{
+            System.out.println(67/0);
+        }
+        finally{
+            System.out.println("done 2");
+        }// ------------------------------> valid
     }
 }
