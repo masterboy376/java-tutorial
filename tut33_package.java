6811b9330fa45b3a2b5026f735642ea28632e5e7
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-26 20:16:36 +0530

    Create tut33_package.java

---
+++ tut33_package.java
@@ -0,0 +1,26 @@
+package practiceset12.prob1;
+
+
+class claculator{
+    public void claculate(int a, int b){
+        System.out.println(" your result is "+ a+b);
+    }
+} 
+class scclaculator{
+    public void claculate(int a, int b){
+        System.out.println(" your result is " + Math.sin(a+b));
+    }
+} 
+class hyclaculator{
+    public void claculate(int a, int b){
+        System.out.println(" your result is "+ a+b);
+        System.out.println(" your result is "+ Math.sin(a+b));
+    }
+} 
+
+public class prob1 {
+    public static void main(String[]args){
+        System.out.println("I am main method");
+        
+    }
+}
