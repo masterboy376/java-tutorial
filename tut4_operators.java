bac7b86289e010318f5ddafac98e2ec37e54c7d8
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-26 17:52:07 +0530

    Create tut4_operators.java

---
+++ tut4_operators.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class operators {
+    public static void main(String[] args) {
+
+        //1.arithmatic operator
+        //int a = 4;
+        // int b1 = 6 % a;// Modulo operator
+        //System.out.println(b1);
+
+        //2.assignment operator
+        //int b = 9;
+        //b += 8;
+        //System.out.println(b);
+
+        //3.comparison logical
+        //System.out.println(64==6);
+
+        //4.logical operator
+        //System.out.println(64>4 || 64<5);
+        //System.out.println(64>4 && 64<5);
+        //System.out.println(64);!4);
+
+        //5.bitwise operator
+        //System.out.println(2&3);
+
+ //       10
+ //       11
+ //    --------
+ //       10
+
+    }
+}
