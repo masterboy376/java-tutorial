97336e7ca1ee86737b9b5da8dbb899332788a294
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-26 17:57:23 +0530

    Create practice2.java

---
+++ practice2.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+public class PracticeSet2 {
+    public static void main(String[] args) {
+        /*//question 1
+        float a = 7.0f/4.0f*9.0f/2.0f;
+        System.out.println(a);*/
+
+        /*//question 2
+        // incryption
+        char grade = 'B';
+        grade = (char)(grade + 8);// casting into char
+        System.out.println(grade);
+        // decrypting
+        grade = (char)(grade-8);//casting into char
+        System.out.println(grade);*/
+
+        /*//question 3
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a>8);*/
+
+        /*//question 4
+        float v = 3f;
+        float u = 7f;
+        float a = 6f;
+        float s = 5f;
+        float sol = (v*v - u*u)/(2*a*s);
+        System.out.println(sol);*/
+
+        /*//question 5
+        int a = 7*49/7 + 35/7;
+        System.out.println(a);*/
+
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        System.out.println(a>90);
+    }
+
+}
