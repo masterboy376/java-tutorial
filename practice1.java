c31dfbe925feb6cf41e307dafcecb5a368c5668f
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-26 17:50:24 +0530

    Create practice1.java

---
+++ practice1.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class PracticeSet1 {
+    public static void main(String[] args) {
+        //question1
+        //int a = 75;
+        //int b = 56;
+        //int c = 45;
+        //int sum = a+b+c;
+        //System.out.println(sum);
+
+        // question2
+        // float s1 = 98;
+        // float s2 = 98;
+        // float s3 = 98;
+        // float cgpa = (s1+s2+s3)/30;
+        //  System.out.println(cgpa);
+
+        //question3
+        //System.out.print("What is your name:- ");
+        //Scanner s = new Scanner(System.in);
+        //String name = s.nextLine();
+        //System.out.println("Hello"+name+"have a good day!");
+
+        //question4
+        //Scanner sc1 = new Scanner(System.in);
+        //System.out.print("Kilometers = ");
+        //float km = sc1.nextFloat();
+        //double miles = km*0.6214;
+        //System.out.print("Mils = ");
+        //System.out.println(miles);
+
+        //question5
+        //Scanner sc = new Scanner(System.in);
+        //System.out.print("Write an integer here ");
+        //boolean bool = sc.hasNextInt();
+        //System.out.println(" Your input is "+bool);
+
+
+    }
+}
