23ac6b1911aa18c8033fe18c170e4f9664c5cf29
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-04-26 17:48:20 +0530

    Create tut3_user_inputs.java

---
+++ tut3_user_inputs.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner; //imported Scanner class:study this in future
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        system.out.println("Takig input from the user");
+        Scanner sc = new Scanner(System.in); // this is a oblect
+        System.out.print("Enter number 1 :- ");
+//        int a = sc.nextInt();
+//        float f1 = sc.nextFloat();
+//        System.out.print("Enter number 2 :- ");
+//        int b = sc.nextInt();
+//        float f2 = sc.nextFloat();
+//        int sum = a+b;
+//        float sum = f1+f2;
+//        System.out.print("The sum of these two numbers is : ");
+//        System.out.println(sum);
+//        System.out.println("integer or not");
+//        boolean b1 = sc.hasNextInt();
+//        System.out.println(b1);
+//        String str = sc.next();
+//        System.out.println("Your name:- ");
+//        String str = sc.nextLine();
+//        System.out.println(str);
+        boolean b2 = sc.hasNextFloat();
+        System.out.println(b2);
+    }
+}
